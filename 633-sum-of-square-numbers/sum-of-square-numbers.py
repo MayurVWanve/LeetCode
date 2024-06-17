e4955a6b05cc181dfb588321aceeb1af77ebd627
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-06-17 12:54:00 -0400

    Time: 88 ms (69.86%) | Memory: 16.5 MB (39.72%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.py
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -1,13 +1,28 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        n = int(math.sqrt(c))
+        # n = int(math.sqrt(c))
 
-        for i in range(n,-1,-1):
-            # print("In loop")
-            r = c - math.pow(i,2)
-            r2 = math.sqrt(r)
-            # print(r2, int(r2), r2 == int(r2))
-            if r2 == int(r2):
+        # for i in range(n,-1,-1):
+        #     # print("In loop")
+        #     r = c - math.pow(i,2)
+        #     r2 = math.sqrt(r)
+        #     # print(r2, int(r2), r2 == int(r2))
+        #     if r2 == int(r2):
+        #         return True
+
+        # return False 
+
+        # Can Do it more efficiently with 2 pointers
+        i,j = 0, int(math.sqrt(c))
+        while i<=j:
+            res = i*i + j*j
+            if res == c:
                 return True
 
-        return False        
+            if res > c:
+                j-=1
+
+            else:
+                i+=1
+
+        return False       
