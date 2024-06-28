bb125a929c326e1c2493b275d8bee984d2aa4744
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-06-28 10:15:08 -0400

    Time: 159 ms (64.94%) | Memory: 16.5 MB (44.43%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,12 +1,12 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        soln = []
+        count = 0
         #Odd Length Strings
         for i in range(len(s)):
             l,r = i,i
             while l>=0 and r < len(s) and s[l] == s[r]:
                 # print("Palindrome Found: ",s[l:r+1] )
-                soln.append(s[l:r+1])
+                count+=1
                 l-=1
                 r+=1
 
@@ -15,10 +15,10 @@ class Solution:
             l,r = i,i+1
             while l>=0 and r < len(s) and s[l] == s[r]:
                 # print("Palindrome Found: ",s[l:r+1] )
-                soln.append(s[l:r+1])
+                count+=1
                 l-=1
                 r+=1
 
-        return len(soln)
+        return count
 
         
