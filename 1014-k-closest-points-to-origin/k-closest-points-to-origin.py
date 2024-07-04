0b07ee683abea5385323f795d79bc9fc626c599b
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-07-04 07:33:04 -0400

    Time: 608 ms (37.98%) | Memory: 23 MB (48.85%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        nums = [(math.sqrt((points[i][0]*points[i][0]) + (points[i][1]* points[i][1])), points[i]) for i in range(len(points))]
+        heapq.heapify(nums)
+        # print(nums)
+        nums = heapq.nsmallest(k, nums)
+        ans = [num[1] for num in nums]
+        return ans
+
+
+        
+        
