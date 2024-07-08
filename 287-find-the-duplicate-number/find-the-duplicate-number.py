a81788bf970fe4f7250fcd809b3dbb6078860ac8
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-07-07 20:42:33 -0400

    Time: 813 ms (5.01%) | Memory: 51.8 MB (5.07%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -0,0 +1,44 @@
+class LinkedList:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        search = {}
+            
+        for i in range(len(nums)):
+            node = LinkedList(i)
+            search[i] = node
+        for i in range(len(nums)):
+            node = search[i]
+            node.next = search[nums[i]]
+
+        slow = fast =  search[0]
+        
+        flag = True
+        while flag or fast != slow:
+            flag = False
+            slow = slow.next
+            fast = fast.next.next
+        
+        slow2 = search[0]
+
+        while slow2 != slow:
+            slow2 = slow2.next
+            slow= slow.next
+        
+
+        return slow.val
+            
+        
+            
+        
+
+            
+
+                
+
+
+
+        
