6ac3a854ed70015e5347cb6067b39ba8420974a8
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-07-10 01:57:35 -0400

    Time: 1281 ms (24.05%) | Memory: 18.5 MB (6.81%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,22 +1,46 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
+        # n = len(nums) DP -> O(n2)
+        # if n == 1:
+        #     return 0
+
+        # dp = [float('inf')] * n
+        # dp[n-1] = 0
+
+        # for i in range(n-2, -1, -1):
+        #     temp = []
+        #     for j in range(1, nums[i]+1):
+        #         if i + j < n:
+        #             temp.append(1+dp[i+j])
+            
+        #     dp[i] = min(temp) if temp else float('inf')
+        
+
+        # return dp[0]
+
+        # Greedy O(n)
+
+        q, n = [(i+1,1) for i in range(nums[0])], len(nums)
+        vis = set([i+1 for i in range(nums[0])])
+
         if n == 1:
             return 0
 
-        dp = [float('inf')] * n
-        dp[n-1] = 0
-
-        for i in range(n-2, -1, -1):
-            temp = []
-            for j in range(1, nums[i]+1):
-                if i + j < n:
-                    temp.append(1+dp[i+j])
+        while q:
+            pos, dis = q.pop(0)
+            if pos == n-1:
+                return dis
             
-            dp[i] = min(temp) if temp else float('inf')
+            for i in range(1,nums[pos]+1):
+                if pos + i not in vis:
+                    vis.add(pos+i)
+                    q.append((pos+i, 1 + dis))
         
+        return -1
 
-        return dp[0]
+
+        
+       
 
 
 
