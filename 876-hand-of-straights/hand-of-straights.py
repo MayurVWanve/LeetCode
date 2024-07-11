45ec6f2a1dffa480a3b813898939594569dd4134
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-07-11 16:50:45 -0400

    Time: 2567 ms (5.00%) | Memory: 18.3 MB (50.80%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        n = len(hand)
+        if n % groupSize != 0:
+            return False
+
+        hand.sort()
+        soln = [[] for i in range(n//groupSize)]
+
+        for i in hand:
+            flag = False
+            for group in soln:
+                if len(group) == 0 or (len(group) < groupSize and (group[-1] +1 ) == i):
+                    group.append(i)
+                    flag = True
+                    break
+            
+            if flag == False:
+                return False
+
+        
+        return True
+                
+        
