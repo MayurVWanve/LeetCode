89e9246ac1fd5ef3a913316e96117ba566944246
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-07-27 18:30:00 -0400

    Time: 20 ms (99.78%) | Memory: 16.4 MB (92.25%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -1,5 +1,15 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        l = [a,b]
-        return sum(l)
+        # l = [a,b]
+        # return sum(l)
+        mask = 0xFFFFFFFF
+
+        while (mask & b) != 0:
+            t = a ^ b
+            b = (a & b) << 1
+            a = t
+
+        return a if b == 0 else (mask & a)
+        
+
         
