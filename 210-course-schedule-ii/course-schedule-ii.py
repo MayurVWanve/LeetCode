13d789baa25f344f5218fae560d7e367d0fc3330
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-07-29 14:12:07 -0400

    Time: 102 ms (15.38%) | Memory: 18.6 MB (10.13%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,31 +1,73 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         # Topological Sort Logic
-        graph, indegree, q,visited, soln   = defaultdict(list), [0 for i in range(numCourses)], deque(), {}, []
+        # graph, indegree, q,visited, soln   = defaultdict(list), [0 for i in range(numCourses)], deque(), {}, []
+
+        # for i, j in prerequisites:
+        #     if i == j:
+        #         return False
+        #     graph[j].append(i)
+        #     indegree[i] += 1
+        
+        # for i in range(numCourses):
+        #     if indegree[i] == 0:
+        #         q.append(i)
+        #         visited[i] = 1
+
+        
+        # while(q):
+        #     node = q.popleft()
+        #     soln.append(node)
+            
+        #     for i in graph[node]:
+        #         indegree[i] -= 1
+                
+        #     for j in range(numCourses):
+        #         if indegree[j] == 0 and j not in visited:
+        #             q.append(j)
+        #             visited[j] = 1
+
+        
+        # return soln if len(visited) == numCourses else []
+
+        graph, vis, soln, finish = defaultdict(list), set(), [], set()
 
         for i, j in prerequisites:
             if i == j:
                 return False
             graph[j].append(i)
-            indegree[i] += 1
+
+        
+        def dfs(node):
+            print(f'Node Considered: {node}')
+            nonlocal soln
+
+            if node in finish:
+                return True
+            
+            if node in vis:
+                return False
+
+            vis.add(node)
+
+            for neigh in graph[node]:
+                print(f'Node and neighbour: {node, neigh}')
+                if not dfs(neigh):
+                    return False
+                
+            
+            vis.remove(node)
+            finish.add(node)
+            soln.append(node)
+            return True
+            
         
         for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)
-                visited[i] = 1
-
+            if i not in finish:
+                if not dfs(i):
+                    return []
         
-        while(q):
-            node = q.popleft()
-            soln.append(node)
-            
-            for i in graph[node]:
-                indegree[i] -= 1
-                
-            for j in range(numCourses):
-                if indegree[j] == 0 and j not in visited:
-                    q.append(j)
-                    visited[j] = 1
+        return soln[::-1]
 
-        
-        return soln if len(visited) == numCourses else []
+
+
