3b1d64330872fa6c6465ca3a1d003cb6a7196d9d
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-08-24 10:04:34 -0400

    Time: 3838 ms (68.73%) | Memory: 16.5 MB (89.72%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -3,15 +3,15 @@ class Solution:
         m, n = len(board), len(board[0])
 
         def dfs(i, j, pos):
-            if i < 0 or j < 0 or i >= m or j >= n or board[i][j] != word[pos]:
+            if i < 0 or j < 0 or i >= m or j >= n or board[i][j] != word[pos] or (i,j) in visited:
                 return False
             
-            if pos == len(word) - 1:
+            if pos == len(word) - 1 and board[i][j] == word[pos]:
                 return True  # Found the word
             
             # Temporarily mark the cell as visited by changing its value
-            temp, board[i][j] = board[i][j], '#'
-            
+            # temp, board[i][j] = board[i][j], '#'
+            visited.add((i,j))
             # Explore all four directions
             found = (dfs(i+1, j, pos+1) or
                      dfs(i-1, j, pos+1) or
@@ -19,12 +19,13 @@ class Solution:
                      dfs(i, j-1, pos+1))
             
             # Restore the cell's original value
-            board[i][j] = temp
+            visited.remove((i,j))
             
             return found
 
         for i in range(m):
             for j in range(n):
+                visited = set()
                 if board[i][j] == word[0] and dfs(i, j, 0):
                     return True  # If found, return True immediately
         
