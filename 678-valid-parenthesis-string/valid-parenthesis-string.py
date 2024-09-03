5c244e76f1cf43f4891b5cd923e12f2770f71157
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-09-02 22:58:06 -0400

    Time: 35 ms (58.35%) | Memory: 16.5 MB (72.99%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -0,0 +1,43 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        stack, count = [], 0
+
+        for i in s:
+            if i == '(' or i == '*':
+                stack.append(i)
+            
+            else:
+                if stack and stack[-1] == '(':
+                    stack.pop()
+                elif stack and stack[-1] == '*':
+                    temp = []
+                    while stack and stack[-1] == '*':
+                        temp.append(stack.pop())
+                    
+                    if stack:
+                        stack.pop()
+                        stack = stack + temp[:]
+                    
+                    else:
+                        stack = stack + temp[:]
+                        stack.pop()
+                else:
+                    return False
+        
+        for i in stack:
+            if i == '(':
+                count+=1
+            else:
+                count = max(0, count-1)
+        
+        # print(stack)
+        if len(stack) == 0 or count==0:
+            return True
+        else:
+            return False
+        
+
+
+            
+            
+        
