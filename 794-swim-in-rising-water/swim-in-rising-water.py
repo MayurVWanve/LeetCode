aa7779b5f7dd1bca874a46973887b193f46df5f7
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-09-02 21:59:46 -0400

    Time: 74 ms (96.93%) | Memory: 17.4 MB (44.50%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,32 +1,27 @@
 from heapq import heappop, heappush, heapify
 class Solution:
-    def swimInWater(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        heap = []
-        vis = set()
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
+        def swimInWater(self, grid: List[List[int]]) -> int:
+            n, heap, vis, directions = len(grid), [], set(), [(0, 1), (1, 0), (-1, 0), (0, -1)] # heap(cost, i, j) cost for that grid[i][j]
 
-        # Start with the top-left corner
-        heapq.heappush(heap, (grid[0][0], 0, 0))
-        vis.add((0, 0))
+            heap.append((grid[0][0], 0,0))
+            vis.add((0,0))
 
-        while heap:
-            cost, x, y = heapq.heappop(heap)
-            
-            # Early exit condition
-            if (x, y) == (n - 1, n - 1):
-                return cost
-            
-            # Explore four possible directions
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < n and 0 <= ny < n and (nx, ny) not in vis:
-                    heapq.heappush(heap, (max(grid[nx][ny], cost), nx, ny))
-                    vis.add((nx, ny))  # Mark as visited when adding to the heap
+            while heap:
+                # print(f'Heap : {heap}')
+                cost, i, j = heappop(heap)
+                vis.add((i,j))
 
-        return -1
-            
+                if (i,j) == (n-1,n-1):
+                    return cost
 
+                for dx, dy in directions:
+                    i_new, j_new = i+dx, j+dy
+                    # print(i_new, j_new)
+                    if 0<=i_new<n and 0<=j_new<n and (i_new, j_new) not in vis:
+                        heappush(heap,(max(grid[i_new][j_new],cost), i_new, j_new))
+                        vis.add((i_new,j_new)) # Because of the fact that you're choosing a node with least cost, every time we add a node to our heap, we are sure that it is the least cost to reach there.
+
+            return -1
             
 
 
