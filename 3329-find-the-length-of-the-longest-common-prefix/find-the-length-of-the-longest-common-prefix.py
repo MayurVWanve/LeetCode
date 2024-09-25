9997ee4fc37825f0941f6453b616bcfe50bd9672
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-09-24 21:03:39 -0400

    Time: 909 ms (61.00%) | Memory: 37.2 MB (7.40%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.py
@@ -1,5 +1,67 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
 class Solution:
+    def __init__(self):
+            self.root = TrieNode()
+            self.soln = 0
+            
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        arr1, arr2 = set(arr1), set(arr2)
+
+        
+
+        def add(ele):
+            curr = self.root
+            for c in ele:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.end = True
+        
+
+        def search(ele):
+            count = 0
+            curr = self.root
+            for c in ele:
+                if c not in curr.children:
+                    return count
+                curr = curr.children[c]
+                count+=1
+            return count
+
+
+        
+        for ele in arr1:
+            add(str(ele))
+        
+        for ele in arr2:
+            count = search(str(ele))
+            self.soln = max(self.soln, count)
+
+        return self.soln
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # soln = 0
         # arr1, arr2 = list(set(arr1)), list(set(arr2))
         # arr1.sort(), arr2.sort()
@@ -25,22 +87,29 @@ class Solution:
 
         # return soln
 
-        if len(arr2) < len(arr1):
-            arr1, arr2 = arr2 , arr1 # We want the smaller list to be in hashset
+        # if len(arr2) < len(arr1):
+        #     arr1, arr2 = arr2 , arr1 # We want the smaller list to be in hashset
 
-        hashset, soln = set(), 0
+        # hashset, soln = set(), 0
 
-        for ele in arr1:
-            while ele and ele not in hashset:
-                # print(ele)
-                hashset.add(ele)
-                ele//=10
+        # for ele in arr1:
+        #     while ele and ele not in hashset:
+        #         # print(ele)
+        #         hashset.add(ele)
+        #         ele//=10
 
-        for ele in arr2:
-            while ele:
-                if ele in hashset:
-                    soln = max(soln, len(str(ele)))
-                    break
-                ele//=10
+        # for ele in arr2:
+        #     while ele:
+        #         if ele in hashset:
+        #             soln = max(soln, len(str(ele)))
+        #             break
+        #         ele//=10
 
-        return soln
+        # return soln
+    
+
+
+        
+            
+        
+ 
