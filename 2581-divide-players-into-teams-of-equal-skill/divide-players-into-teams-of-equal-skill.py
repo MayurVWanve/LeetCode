6a52744cb33dbe582b7fdf64acb0d7da04b825e6
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-10-03 21:29:21 -0400

    Time: 407 ms (43.36%) | Memory: 31 MB (6.90%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,23 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        n = len(skill)
+        need = (sum(skill) *2)//n
+        table, soln= defaultdict(int), []
+
+        for ele in skill:
+            # print(f'ele:{ele}, table:{table}, soln:{soln}')
+            num = need - ele
+            if num in table:
+                soln.append(num*ele)
+                if table[num] != 1:
+                    table[num]-=1
+                else:
+                    del(table[num])
+
+            else:
+                table[ele]+=1
+            
+        print(soln)
+        return -1 if len(soln) != n//2 else sum(soln)
+
+        
