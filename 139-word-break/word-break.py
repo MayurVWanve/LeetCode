6bd646fb2c99a985367827fc99d1955d96f652e5
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2024-11-10 11:14:06 -0500

    Time: 2 ms (75.57%) | Memory: 16.8 MB (36.84%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,41 +1,23 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # Base case
-        # flag, cache = False, {}
+        cache = {} # {postion -> bool}
 
-        # if s == '':
-        #     return True
-        
-        # for word in wordDict:
-        #     n = len(word)
-        #     if s[:n] == word:
-        #         flag = self.wordBreak(s[n:], wordDict)
-        #         cache[n]
-        #         if flag == True:
-        #             return flag
-        
-        # return flag
-
-        cache, n = {}, len(s)
-
-        def helper(pos):
-            flag = False
+        def backtrack(pos):
+            if pos >= len(s):
+                return True
 
             if pos in cache:
                 return cache[pos]
 
-            if pos == n:
-                return True
-
             for word in wordDict:
-                m = len(word)
-                if s[pos: pos + m] == word:
-                    flag = helper(pos+m)
-                    cache[pos+m] = flag
-                    if flag:
-                        cache[pos] = flag
-                        return flag
+                if word == s[pos:pos + len(word)]:
+                    if backtrack(pos+len(word)):
+                        cache[pos+len(word)] = True
+                        return True
+            
+            cache[pos] = False
+            return cache[pos]
 
-            return flag
+        return (backtrack(0))
 
-        return helper(0)
+        
