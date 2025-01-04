787ae0261f2ecdabe1f5e5838b0c9218cf133a4d
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2025-01-04 18:17:08 -0500

    Time: 99 ms (88.84%) | Memory: 18.3 MB (25.38%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,18 +1,28 @@
 from collections import Counter
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        left, right, soln = set(), Counter(s), set()
+    #     left, right, soln = set(), Counter(s), set()
         
-        for i in range(len(s)):
-            right[s[i]] -=1
-            if right[s[i]] == 0:
-                del(right[s[i]])
+    #     for i in range(len(s)):
+    #         right[s[i]] -=1
+    #         if right[s[i]] == 0:
+    #             del(right[s[i]])
             
-            for j in range(26):
-                m = chr(ord('a') + j)
-                if m in left and m in right:
-                    soln.add((m, s[i]))
+    #         for j in range(26):
+    #             m = chr(ord('a') + j)
+    #             if m in left and m in right:
+    #                 soln.add((m, s[i]))
                 
-            left.add(s[i])
+    #         left.add(s[i])
 
-        return len(soln)
+    #     return len(soln)
+
+        count = Counter(s)  # Count of each letter
+        total = 0
+        for letter in count.keys():
+            if count[letter]>=2:
+                first=s.find(letter)
+                last=s.rfind(letter)
+                uniquechars=set(s[first+1:last])
+                total+=len(uniquechars)
+        return total
