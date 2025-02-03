f64e20f7e463e51c0f20e2b0111e6cff8a72a303
Mayur Wanve <95929210+MayurVWanve@users.noreply.github.com>
2025-02-03 15:10:39 -0600

    Time: 3 ms (51.34%) | Memory: 17.6 MB (69.83%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -6,22 +6,22 @@ class Solution:
         if n == 1:
             return 1
         
-        window, i = 1, 0  # Initialize window to at least 1
+        window, i = 1, 0  
         flag = 0  # 0: uninitialized, 1: increasing, -1: decreasing
         
         for j in range(1, n):
-            if nums[j] > nums[j - 1]:  # Increasing sequence
-                if flag == -1:  # Previously decreasing
-                    i = j - 1  # Reset start of new subarray
+            if nums[j] > nums[j - 1]:  #increasing
+                if flag == -1: # decreasing
+                    i = j - 1 
                 flag = 1
-            elif nums[j] < nums[j - 1]:  # Decreasing sequence
-                if flag == 1:  # Previously increasing
-                    i = j - 1  # Reset start of new subarray
+            elif nums[j] < nums[j - 1]:  
+                if flag == 1:  
+                    i = j - 1  
                 flag = -1
-            else:  # Same value, reset
+            else: 
                 flag = 0
                 i = j
             
-            window = max(window, j - i + 1)  # Update longest window
+            window = max(window, j - i + 1)  
         
         return window
